cef15b7dc4b08796680da481b0b667ab28c20d84
eek#7574 <eek@clicksminuteper.net>
2021-09-27 14:19:09 +0100

    Allow vercel to use database again

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,13 @@ const KeyV = require("keyv");
 let db;
 
 if(process.env.VERCEL==="1") {
-    db = new KeyV("sqlite://:memory:");
+    console.debug("Running on vercel - using memory cache or database.")
+    db = new KeyV();
 }
 else {
-    db = new KeyV("sqlite://keyv.db")
-}
+    const uri = process.env.DB_URI || "sqlite://keyv.db";
+    console.debug("Running on a local machine - connecting to " + uri)
+    db = new KeyV(uri);
+};
 
 export {db};
